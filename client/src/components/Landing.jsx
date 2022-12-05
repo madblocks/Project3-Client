04f6306900d0c6d5fdbb8cdeacc9e4222155be92
tim villanueva <timvillanueva3@gmail.com>
2022-12-05 09:07:40 -0600

    map sadness

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,11 +1,9 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup, LayersControl, LayerGroup } from 'react-leaflet'
-import {icon, map} from 'leaflet'
+import { MapContainer, TileLayer, Marker, Popup, LayersControl, LayerGroup } from 'react-leaflet'
 import styled from 'styled-components'
-import { useState, useContext, useEffect } from 'react'
+import { useState, useContext, useEffect, useRef, useMemo} from 'react'
 import  Button  from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import { DataContext } from '../DataContext' 
-import axios from 'axios'
 import Client from '../services/api'
 
 const StyledWrapper = styled.div `
@@ -35,10 +33,12 @@ const StyledWrapper = styled.div `
     display:flex;
     flex-direction: row;
 }`;
-
 const Landing = (props) =>{
 
+ 
 
+
+    const mapRef = useRef();
     const {isLoggedIn, setLoggedIn} = useContext(DataContext)
     const [searchCriteria, setSearchCriteria] = useState([])
     const [activeEvent, setActiveEvent] = useState(null)
@@ -46,6 +46,7 @@ const Landing = (props) =>{
     const [allEvents, setAllEvents] = useState([])
     const [mapRendered, setMapRendered] = useState(0)
     const [loading, setLoading] = useState(true)
+
     const [hiking, setHiking] = useState([])
     const [running, setRunning] = useState([])
     const [ultimate, setUltimate] = useState([])
@@ -58,6 +59,10 @@ const Landing = (props) =>{
     const [birdWatching, setBirdWatching] = useState([])
     const eventsArray = [hiking, running, ultimate, skiing, mountainBiking, roadBiking, kayaking, rafting, fishing, birdWatching]
     
+    const [map, setMap] = useState(null)
+    const [layersReady, setLayersReady] = useState(false)
+    const [layers, setLayers] = useState(null)
+
     const detailsStyle = {
         border: "2px solid black",
         width: "20vw",
@@ -85,75 +90,17 @@ const Landing = (props) =>{
     const handleShow = () => setShowDetails(true);
     const handleClose = () => setShowDetails(false)
 
-useEffect(()=> {
-const handleEvents = async () => {
-    const getEvents = async () => {
-    try {
-        const res = await Client.get('api/event')
-        return res.data
-    } catch (error) {throw error}
-    }
-    const events = await getEvents();
-    setAllEvents(events)
-}
-
-const sortEvents =  (array) => {
-    
-    for (let i=0; i<array.length; i++){
-            if (array[i].activityId === 1) {
-                setHiking(hiking => [...hiking, array[i]])
-            } else if (array[i].activityId === 2) {
-                setRunning(running => [...running, array[i]])
-            } else if (array[i].activityId === 3) {
-                setUltimate(running => [...running, array[i]])
-                ultimate.push(array[i]);
-            } else if (array[i].activityId === 4) {
-                setSkiing(skiing => [...skiing, array[i]])
-            } else if (array[i].activityId === 5) {
-                setMountainBiking(mountainBiking => [...mountainBiking, array[i]])
-            } else if (array[i].activityId === 6) {
-                setRoadBiking(roadBiking => [...roadBiking, array[i]])
-            } else if (array[i].activityId === 7) {
-                setKayaking(kayaking => [...kayaking, array[i]])
-            } else if (array[i].activityId === 8) {
-                setRafting(rafting => [...rafting, array[i]])
-            } else if (array[i].activityId === 9) {
-                setFishing(fishing => [...fishing, array[i]])
-            } else if (array[i].activityId === 10) {
-                setBirdWatching(birdWatching => [...birdWatching, array[i]])
-            }
-        }
-    setLoading(false)
-    console.log(fishing)
-    }
-
-handleEvents()
-sortEvents(allEvents);
-
-
-},[mapRendered])
-
 
 const updateMap =() => {
     setMapRendered(mapRendered+1);
-}
+    }
 
+const createLayers = () => {
 
-    return loading ? (  <div><h1>Loading...</h1>
-    <button onClick = {updateMap}>update map</button></div>   ) : 
-    <StyledWrapper>
-    <div className="landing-container">
-        <input type="text" placeholder="search" className="search"></input>need search button and create event button
-
-        <h6 className='instructions'>click and drag to move, use scrollwheel to zoom</h6><button onClick = {updateMap}>update map</button>
-        <div className="map-and-details">
-        <MapContainer center={[35.591, -82.55]} zoom={11} className="map">
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        return allEvents.length > 0 ? (
         <LayersControl position="topright">
-        {eventsArray.map(event => (
-            <LayersControl.Overlay checked name={event} key = {event.id}>
+        {eventsArray.map((event, index) => (
+            <LayersControl.Overlay checked name={`${event[0].name}(${event.length})`} key = {index} layerId = {index}>
                 <LayerGroup >
                 {event.map(activity => (
                     <Marker key={activity.id} position={[activity.latitude, activity.longitude]}>
@@ -171,7 +118,88 @@ const updateMap =() => {
             </LayersControl.Overlay>
             ))}
         </LayersControl>
-        </MapContainer>
+        ) : false
+    }
+
+
+useEffect(()=>{
+const handleEvents = async () => {
+
+    const sortEvents =  (array) => {
+    
+        for (let i=0; i<array.length; i++){
+                if (array[i].activityId === 1) {
+                    setHiking(hiking => [...hiking, array[i]])
+                } else if (array[i].activityId === 2) {
+                    setRunning(running => [...running, array[i]])
+                } else if (array[i].activityId === 3) {
+                    setUltimate(ultimate => [...ultimate, array[i]])
+                } else if (array[i].activityId === 4) {
+                    setSkiing(skiing => [...skiing, array[i]])
+                } else if (array[i].activityId === 5) {
+                    setMountainBiking(mountainBiking => [...mountainBiking, array[i]])
+                } else if (array[i].activityId === 6) {
+                    setRoadBiking(roadBiking => [...roadBiking, array[i]])
+                } else if (array[i].activityId === 7) {
+                    setKayaking(kayaking => [...kayaking, array[i]])
+                } else if (array[i].activityId === 8) {
+                    setRafting(rafting => [...rafting, array[i]])
+                } else if (array[i].activityId === 9) {
+                    setFishing(fishing => [...fishing, array[i]])
+                } else if (array[i].activityId === 10) {
+                    setBirdWatching(birdWatching => [...birdWatching, array[i]])
+                }
+            }
+        setLoading(false)
+    
+        }
+
+    const getEvents = async () => {
+    try {
+        const res = await Client.get('api/event')
+        return res.data
+    } catch (error) {throw error}
+    }
+    const layerGenerator = () => {
+        const temp = createLayers()
+        if (!temp){
+            console.log('did not generate layers')
+        }
+        else {
+        setLayers(temp)
+        setLayersReady(true)
+        }
+    }
+
+try{const events = await getEvents();
+try{setAllEvents(events)}catch (error){ throw error}
+try{ sortEvents(allEvents)}catch (error){ throw error}
+try{layerGenerator()}catch (error){ throw error}
+
+} catch (error) {throw error}
+
+}
+handleEvents()
+},[mapRendered])
+
+
+return loading && !layersReady ? (  <div><h1>Loading...</h1>
+    <button onClick = {updateMap}>update map</button></div>   ) : 
+    
+    <StyledWrapper>
+    <div className="landing-container">
+        <input type="text" placeholder="search" className="search"></input>need search button and create event button
+
+        <h6 className='instructions'>click and drag to move, use scrollwheel to zoom</h6><button onClick = {updateMap}>update map</button>
+        <div className="map-and-details">
+        
+    <MapContainer center={[35.591, -82.55]} zoom={11} className="map" ref={setMap}>
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            {layers}
+    </MapContainer>
+
         <Modal show={showDetails} onHide={handleClose} style={detailsStyle}>
             <Modal.Header>
                 <Modal.Title>Activity Name<Button onClick={handleClose} style={{float: "right"}}>close</Button></Modal.Title>
@@ -186,9 +214,7 @@ const updateMap =() => {
                 <Button onClick={adjustLike}>Like</Button>
             </Modal.Body>
         </Modal>
-
         </div>
-        
     </div>
     </StyledWrapper>
 
